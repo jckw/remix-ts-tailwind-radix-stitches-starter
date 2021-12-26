278344cab1a456d25e7c4a14f0e8faff7dfa1bd4
Jack Weatherilt <jackweatherilt@outlook.com>
2021-12-26 21:40:08 +0000

    Include deps and format.

--- app/root.tsx
+++ app/root.tsx
@@ -6,24 +6,24 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useCatch
-} from "remix";
-import type { LinksFunction } from "remix";
+  useCatch,
+} from 'remix'
+import type { LinksFunction } from 'remix'
 
-import globalStylesUrl from "~/styles/global.css";
-import darkStylesUrl from "~/styles/dark.css";
+import globalStylesUrl from '~/styles/global.css'
+import darkStylesUrl from '~/styles/dark.css'
 
 // https://remix.run/api/app#links
 export let links: LinksFunction = () => {
   return [
-    { rel: "stylesheet", href: globalStylesUrl },
+    { rel: 'stylesheet', href: globalStylesUrl },
     {
-      rel: "stylesheet",
+      rel: 'stylesheet',
       href: darkStylesUrl,
-      media: "(prefers-color-scheme: dark)"
-    }
-  ];
-};
+      media: '(prefers-color-scheme: dark)',
+    },
+  ]
+}
 
 // https://remix.run/api/conventions#default-export
 // https://remix.run/api/conventions#route-filenames
@@ -34,12 +34,12 @@ export default function App() {
         <Outlet />
       </Layout>
     </Document>
-  );
+  )
 }
 
 // https://remix.run/docs/en/v1/api/conventions#errorboundary
 export function ErrorBoundary({ error }: { error: Error }) {
-  console.error(error);
+  console.error(error)
   return (
     <Document title="Error!">
       <Layout>
@@ -54,14 +54,14 @@ export function ErrorBoundary({ error }: { error: Error }) {
         </div>
       </Layout>
     </Document>
-  );
+  )
 }
 
 // https://remix.run/docs/en/v1/api/conventions#catchboundary
 export function CatchBoundary() {
-  let caught = useCatch();
+  let caught = useCatch()
 
-  let message;
+  let message
   switch (caught.status) {
     case 401:
       message = (
@@ -69,16 +69,16 @@ export function CatchBoundary() {
           Oops! Looks like you tried to visit a page that you do not have access
           to.
         </p>
-      );
-      break;
+      )
+      break
     case 404:
       message = (
         <p>Oops! Looks like you tried to visit a page that does not exist.</p>
-      );
-      break;
+      )
+      break
 
     default:
-      throw new Error(caught.data || caught.statusText);
+      throw new Error(caught.data || caught.statusText)
   }
 
   return (
@@ -90,15 +90,15 @@ export function CatchBoundary() {
         {message}
       </Layout>
     </Document>
-  );
+  )
 }
 
 function Document({
   children,
-  title
+  title,
 }: {
-  children: React.ReactNode;
-  title?: string;
+  children: React.ReactNode
+  title?: string
 }) {
   return (
     <html lang="en">
@@ -113,10 +113,10 @@ function Document({
         {children}
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
-  );
+  )
 }
 
 function Layout({ children }: { children: React.ReactNode }) {
@@ -151,7 +151,7 @@ function Layout({ children }: { children: React.ReactNode }) {
         </div>
       </footer>
     </div>
-  );
+  )
 }
 
 function RemixLogo() {
@@ -174,5 +174,5 @@ function RemixLogo() {
       <path d="M478.436 47.104V161.28H519.908V47.104H478.436ZM478.18 36.352H520.164V0H478.18V36.352Z" />
       <path d="M654.54 47.1035H611.788L592.332 74.2395L573.388 47.1035H527.564L568.78 103.168L523.98 161.28H566.732L589.516 130.304L612.3 161.28H658.124L613.068 101.376L654.54 47.1035Z" />
     </svg>
-  );
+  )
 }
--- app/routes/demos/params/$id.tsx
+++ app/routes/demos/params/$id.tsx
@@ -1,5 +1,5 @@
-import { useCatch, Link, json, useLoaderData } from "remix";
-import type { LoaderFunction, MetaFunction } from "remix";
+import { useCatch, Link, json, useLoaderData } from 'remix'
+import type { LoaderFunction, MetaFunction } from 'remix'
 
 // The `$` in route filenames becomes a pattern that's parsed from the URL and
 // passed to your loaders so you can look up data.
@@ -7,52 +7,52 @@ import type { LoaderFunction, MetaFunction } from "remix";
 export let loader: LoaderFunction = async ({ params }) => {
   // pretend like we're using params.id to look something up in the db
 
-  if (params.id === "this-record-does-not-exist") {
+  if (params.id === 'this-record-does-not-exist') {
     // If the record doesn't exist we can't render the route normally, so
     // instead we throw a 404 reponse to stop running code here and show the
     // user the catch boundary.
-    throw new Response("Not Found", { status: 404 });
+    throw new Response('Not Found', { status: 404 })
   }
 
   // now pretend like the record exists but the user just isn't authorized to
   // see it.
-  if (params.id === "shh-its-a-secret") {
+  if (params.id === 'shh-its-a-secret') {
     // Again, we can't render the component if the user isn't authorized. You
     // can even put data in the response that might help the user rectify the
     // issue! Like emailing the webmaster for access to the page. (Oh, right,
     // `json` is just a Response helper that makes it easier to send JSON
     // responses).
-    throw json({ webmasterEmail: "hello@remix.run" }, { status: 401 });
+    throw json({ webmasterEmail: 'hello@remix.run' }, { status: 401 })
   }
 
   // Sometimes your code just blows up and you never anticipated it. Remix will
   // automatically catch it and send the UI to the error boundary.
-  if (params.id === "kaboom") {
-    lol();
+  if (params.id === 'kaboom') {
+    lol()
   }
 
   // but otherwise the record was found, user has access, so we can do whatever
   // else we needed to in the loader and return the data. (This is boring, we're
   // just gonna return the params.id).
-  return { param: params.id };
-};
+  return { param: params.id }
+}
 
 export default function ParamDemo() {
-  let data = useLoaderData();
+  let data = useLoaderData()
   return (
     <h1>
-      The param is <i style={{ color: "red" }}>{data.param}</i>
+      The param is <i style={{ color: 'red' }}>{data.param}</i>
     </h1>
-  );
+  )
 }
 
 // https://remix.run/api/conventions#catchboundary
 // https://remix.run/api/remix#usecatch
 // https://remix.run/api/guides/not-found
 export function CatchBoundary() {
-  let caught = useCatch();
+  let caught = useCatch()
 
-  let message: React.ReactNode;
+  let message: React.ReactNode
   switch (caught.status) {
     case 401:
       message = (
@@ -60,11 +60,11 @@ export function CatchBoundary() {
           Looks like you tried to visit a page that you do not have access to.
           Maybe ask the webmaster ({caught.data.webmasterEmail}) for access.
         </p>
-      );
+      )
+      break
     case 404:
-      message = (
-        <p>Looks like you tried to visit a page that does not exist.</p>
-      );
+      message = <p>Looks like you tried to visit a page that does not exist.</p>
+      break
     default:
       message = (
         <p>
@@ -72,7 +72,7 @@ export function CatchBoundary() {
           <br />
           {caught.status} {caught.statusText}
         </p>
-      );
+      )
   }
 
   return (
@@ -84,13 +84,13 @@ export function CatchBoundary() {
         link in the parts of the UI that didn't blow up?)
       </p>
     </>
-  );
+  )
 }
 
 // https://remix.run/api/conventions#errorboundary
 // https://remix.run/api/guides/not-found
 export function ErrorBoundary({ error }: { error: Error }) {
-  console.error(error);
+  console.error(error)
   return (
     <>
       <h2>Error!</h2>
@@ -100,11 +100,11 @@ export function ErrorBoundary({ error }: { error: Error }) {
         link in the parts of the UI that didn't blow up?)
       </p>
     </>
-  );
+  )
 }
 
 export let meta: MetaFunction = ({ data }) => {
   return {
-    title: data ? `Param: ${data.param}` : "Oops...",
-  };
-};
+    title: data ? `Param: ${data.param}` : 'Oops...',
+  }
+}
